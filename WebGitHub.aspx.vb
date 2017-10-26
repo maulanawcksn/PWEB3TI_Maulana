a11005543b4fc985b3543ecbacffa5c17b89de1d
Maulana Wicaksono <33107407+maulanawcksn@users.noreply.github.com>
2017-10-26 09:36:22 +0700

    Delete WebGitHub.aspx.vb

--- WebGitHub.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Partial Class GitHub_WebGitHub
-    Inherits System.Web.UI.Page
-
-    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        out.Text = "Nama anda " & txtnama.Text & " <br> Nim anda " & txtnim.Text & "<br> Prodi anda " & txtprodi.Text
-    End Sub
-End Class
